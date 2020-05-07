6329a034ce6f45f8ecd4032be3d54ea145840f8b
Edwin J. Estrella Ayala <edwinjestrella@yahoo.com>
2020-05-07 00:35:59 -0400

    Interfaces

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { User } from '../../models/User';
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
@@ -7,53 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserComponent implements OnInit {
   // Properties
-  firstName: string;
-  lastName: string;
-  age: number;
-  address;
-
-  foo: any;
-  hasKids: boolean;
-  numArr: number[];
-  strArr: string[];
-  mixedArr: any[];
-  myTuple: [string, number, boolean];
-  noRet: void;
-  uD: undefined;
-  n: null;
-
+  user: User;
   // Constructor
   constructor() {
-    this.firstName = 'Edwin';
-    this.lastName = 'Estrella';
-    this.age = 25;
-
-    this.address = {
-      street: 'Calle Tamarindo',
-      city: 'San Antonio',
-      state: 'PR',
+    this.user = {
+      firstName: 'Edwin',
+      lastName: 'Estrella',
+      age: 25,
+      address: {
+        street: 'Calle Tamarindo',
+        city: 'San Antonio',
+        state: 'PR',
+      },
     };
-
-    this.foo = true;
-    this.hasKids = false;
-
-    this.numArr = [1, 2, 3];
-    this.strArr = ['Hello', 'This'];
-    this.mixedArr = [1, 2, 'hola', false];
-    this.myTuple = ['h', 1.2, false];
-    this.noRet = undefined;
-    this.uD = undefined;
-    this.n = null;
-
-    console.log(this.addNums(4, 5));
-  }
-
-  addNums(num1: number, num2: number): number {
-    return num1 + num2;
-  }
-
-  showAge() {
-    return this.age;
   }
 
   ngOnInit(): void {}
