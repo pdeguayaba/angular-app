05cd88676cc9c047be4027032fd530d316b2e775
Edwin J. Estrella Ayala <edwinjestrella@yahoo.com>
2020-05-06 23:43:27 -0400

    Properties and methods

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -3,13 +3,29 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
-  styleUrls: ['./user.component.css']
+  styleUrls: ['./user.component.css'],
 })
 export class UserComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  // Constructor
+  constructor() {
+    //this.sayHello();
+    console.log('Before:' + this.age);
+    this.hasBirthday();
+    console.log('Now:' + this.age);
   }
 
+  sayHello() {
+    console.log(`Hello ${this.firstName}`);
+  }
+
+  hasBirthday() {
+    this.age += 1;
+  }
+
+  // Properties
+  firstName = 'Edwin';
+  lastName = 'Estrella';
+  age = 25;
+
+  ngOnInit(): void {}
 }
