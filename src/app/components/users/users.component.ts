d6ad83ea9ff30f661ad54db8b32adc84c637fe18
Edwin J. Estrella Ayala <edwinjestrella@yahoo.com>
2020-05-07 22:14:35 -0400

    Style binding ngStyle

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -13,6 +13,7 @@ export class UsersComponent implements OnInit {
   loaded: boolean = false;
   enableAdd: boolean = true;
   currentClasses = {};
+  currentStyles = {};
 
   constructor() {}
 
@@ -64,6 +65,7 @@ export class UsersComponent implements OnInit {
     });
 
     this.setCurrentClasses();
+    this.setCurrentStyles();
   }
 
   addUser(user: User) {
@@ -74,6 +76,13 @@ export class UsersComponent implements OnInit {
     this.currentClasses = {
       'btn-success': this.enableAdd,
       'big-text': this.showExtended,
+    };
+  }
+
+  setCurrentStyles() {
+    this.currentStyles = {
+      'padding-top': this.showExtended ? '0' : '40px',
+      'font-size': this.showExtended ? '' : '40px',
     };
   }
 }
