0d9f4d5a834c3131be64038588dd59f5f9e47a77
Edwin J. Estrella Ayala <edwinjestrella@yahoo.com>
2020-05-09 12:51:39 -0400

    Template driven forms submission

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { User } from '../../models/User';
 import { flatten } from '@angular/compiler';
 
@@ -18,6 +18,7 @@ export class UsersComponent implements OnInit {
   loaded: boolean = false;
   enableAdd: boolean = false;
   showUserForm: boolean = false;
+  @ViewChild('userForm') form: any;
 
   constructor() {}
 
@@ -44,20 +45,17 @@ export class UsersComponent implements OnInit {
     this.loaded = true;
   }
 
-  // addUser() {
-  //   this.user.isActive = true;
-  //   this.user.registered = new Date();
-  //   this.users.unshift(this.user);
+  onSubmit({ value, valid }: { value: User; valid: boolean }) {
+    if (!valid) {
+      console.log('Not valid');
+    } else {
+      value.isActive = true;
+      value.registered = new Date();
+      value.hide = true;
 
-  //   this.user = {
-  //     firstName: '',
-  //     lastName: '',
-  //     email: '',
-  //   };
-  // }
+      this.users.unshift(value);
 
-  onSubmit(e) {
-    console.log(123);
-    e.preventDefault();
+      this.form.reset();
+    }
   }
 }
