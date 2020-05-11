2fece8fc5e481e2996f2c6930dc3705af0769289
Edwin J. Estrella Ayala <edwinjestrella@yahoo.com>
2020-05-11 16:46:54 -0400

    Generated posts component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,10 @@ import { UserComponent } from './components/user/user.component';
 import { UsersComponent } from './components/users/users.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { UserService } from './services/user.service';
+import { PostsComponent } from './components/posts/posts.component';
 
 @NgModule({
-  declarations: [AppComponent, UserComponent, UsersComponent, NavbarComponent],
+  declarations: [AppComponent, UserComponent, UsersComponent, NavbarComponent, PostsComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [UserService],
   bootstrap: [AppComponent],
