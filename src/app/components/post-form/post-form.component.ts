5a206a103447a72405cfd0c45e89ef05f52421a0
Edwin J. Estrella Ayala <edwinjestrella@yahoo.com>
2020-05-12 15:25:59 -0400

    Client PUT request

--- src/app/components/post-form/post-form.component.ts
+++ src/app/components/post-form/post-form.component.ts
@@ -10,6 +10,7 @@ import { Post } from '../../models/Post';
 })
 export class PostFormComponent implements OnInit {
   @Output() newPost: EventEmitter<Post> = new EventEmitter();
+  @Output() updatedPost: EventEmitter<Post> = new EventEmitter();
   @Input() currentPost: Post;
   @Input() isEdit: boolean;
 
@@ -28,6 +29,10 @@ export class PostFormComponent implements OnInit {
   }
 
   updatePost() {
-    console.log(123);
+    this.postService.updatePost(this.currentPost).subscribe((post) => {
+      console.log(890);
+      this.isEdit = false;
+      this.updatedPost.emit(post);
+    });
   }
 }
